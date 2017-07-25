1b8448146dba8abc64825b496a14c889b0d70249
Kevin Hahn <hahn.kev@gmail.com>
2017-07-24 17:30:43 -0700

    add login component

---
+++ Frontend/src/app/login/login.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/delay';
+
+@Injectable()
+export class LoginService implements CanActivate {
+  public loggedIn = false;
+  public redirectTo: string;
+  constructor(private router: Router) { }
+  promptLogin(redirectTo?: string) {
+    this.redirectTo = redirectTo;
+    this.router.navigate(['/login']);
+  }
+  async login(username: string, password: string): Promise<boolean> {
+    //todo actually login
+    this.loggedIn = await Observable.of(true).delay(1000).toPromise();
+    if (this.loggedIn) {
+      this.router.navigate([this.redirectTo || '']);
+    }
+    return this.loggedIn;
+  }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+    if (this.loggedIn) {
+      return true;
+    }
+    this.promptLogin(state.url);
+    return false;
+  }
+}
