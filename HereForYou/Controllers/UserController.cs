820f8d455f0b3f0cbda514ae0f3e7701e6302d97
Kevin Hahn <hahn.kev@gmail.com>
2017-07-24 20:07:01 -0700

    add user and controller to backend

---
+++ HereForYou/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HereForYou.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HereForYou.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private static readonly List<User> _users = new List<User>
+        {
+            new User {Name = "Tim", PhoneNumber = "123"},
+            new User {Name = "Timitha", PhoneNumber = "456"}
+        };
+
+        public UserController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return _users;
+        }
+
+//
+//        public User Get(string name)
+//        {
+//            return _users.FirstOrDefault(user => user.Name.Equals(name, StringComparison.CurrentCultureIgnoreCase));
+//        }
+        [HttpPut]
+        public User Put([FromBody] User user)
+        {
+            var saved = false;
+            for (var i = 0; i < _users.Count; i++)
+            {
+                var eachUser = _users[i];
+                if (!eachUser.Name.Equals(user.Name, StringComparison.CurrentCultureIgnoreCase)) continue;
+                saved = true;
+                _users[i] = user;
+                break;
+            }
+            if (!saved) _users.Add(user);
+            return user;
+        }
+    }
+}
---
+++ HereForYou/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace HereForYou.Entities
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool RideProvider { get; set; }
+        /// <summary>
+        /// Password is only used to set the password, it can not be returned from the api
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
